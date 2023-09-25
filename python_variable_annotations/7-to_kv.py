22412bcb696a03d9b610b857c8c214244efd805d
soO <444deph12@gmail.com>
2023-09-25 03:13:23 +0000

    task 7 & 8

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,7 +2,7 @@
 """Basic annotations - to_kv"""
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple"""
     return (k, v * v)
-
--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""Basic annotations - make multiplication"""
+"""Basic annotations - make multiplier"""
+from typing import Callable  # Import Callable from the typing module
 
 
-def make_multiplier(multiplier: float) -> callable[[float], float]:
-    """Return a function"""
-    def f(n: float):
-        return (n * multiplier)
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function that multiplies its input by the multiplier."""
+    def f(n: float) -> float:
+        return n * multiplier
     return f
