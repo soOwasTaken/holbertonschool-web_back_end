0e635f88fa710afc9f6a12c8a01a5deffed3e77d
soO <444deph12@gmail.com>
2023-09-25 03:34:34 +0000

    add of task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Basic annotations - async"""
 import asyncio
-import random
-from 0-basic_async_syntax import wait_random
+from typing import List
+wait_random = __import__("0-basic_async_syntax").wait_random
 
-async def wait_n(n: int, max_delay: int) -> float:
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Wait for a random delay between 0 and max_delay"""
-    tasks = [asyncio.create_task(wait_random.wait_random(max_delay)) for _ in range(n)]
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
