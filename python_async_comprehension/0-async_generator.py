58e1d0770e9a6ae4c5e51c5f997ec6529dde55c5
soO <444deph12@gmail.com>
2023-09-25 04:08:34 +0000

    task 0 ?

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
