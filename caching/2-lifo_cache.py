9e60e377aac1cd567f8d62f08c550a1a99f4a487
soO <444deph12@gmail.com>
2023-09-25 04:28:37 +0000

    fixing task 2

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,32 +1,38 @@
 #!/usr/bin/python3
-""" LIFOCache module """
+""" LRUCache module """
 from base_caching import BaseCaching
+from collections import deque
 
 
-class LIFOCache(BaseCaching):
-    """ LIFOCache class. Inherits from BaseCaching.
-        Uses LIFO algorithm.
+class LRUCache(BaseCaching):
+    """ LRUCache class. Inherits from BaseCaching.
+        Uses LRU algorithm.
     """
 
     def __init__(self):
         super().__init__()
-        self.stack = []
+        self.lru_order = deque()
 
     def put(self, key, item):
         """ Put an item in the cache """
         if key is None or item is None:
             return
 
+        if key in self.lru_order:
+            self.lru_order.remove(key)
+        self.lru_order.append(key)
+
         self.cache_data[key] = item
 
-        if key not in self.stack:
-            self.stack.append(key)
-
-        if len(self.stack) > BaseCaching.MAX_ITEMS:
-            discarded_key = self.stack.pop()
+        if len(self.lru_order) > BaseCaching.MAX_ITEMS:
+            discarded_key = self.lru_order.popleft()
             del self.cache_data[discarded_key]
             print(f"DISCARD: {discarded_key}")
 
     def get(self, key):
         """ Get an item by key """
-        return self.cache_data.get(key, None)
+        value = self.cache_data.get(key, None)
+        if value is not None:
+            self.lru_order.remove(key)
+            self.lru_order.append(key)
+        return value
