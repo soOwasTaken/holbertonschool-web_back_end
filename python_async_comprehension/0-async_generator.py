7c78353dfffdbf7046080581503bdec9d0b9dec0
soO <444deph12@gmail.com>
2023-09-25 04:05:17 +0000

    task 0 ?

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,11 +2,10 @@
 """Async Generator"""
 import asyncio
 import random
-from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """Yield a random number between 0 and 10"""
+async def async_generator():
+    """Yield a random number between 0 and 10 every second, 10 times."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
