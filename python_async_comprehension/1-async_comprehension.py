03623a8866cbc3522a1a9ed51fd00db1ee20c481
soO <444deph12@gmail.com>
2023-09-25 03:56:55 +0000

    task 1 fixed pycodestyle

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,6 +4,7 @@ import asyncio
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     """Collect 10 random numbers using an async comprehensing over async_generator"""
     return [number async for number in async_generator()]
