438a81c3250394b98ec0bdd989dbad02a6edc683
soO <444deph12@gmail.com>
2023-09-25 02:43:41 +0000

    0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-""" takes a float a and a float b
-as arguments and returns their sum as a float. """
 
 
 def add(num1: float, num2: float) -> float:
+    """Add two floats and return the result"""
     return (num1 + num2)
