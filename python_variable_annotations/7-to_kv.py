b0f91a5552f90062ec50ef2062db7fa75f3b8e6d
soO <444deph12@gmail.com>
2023-09-25 03:11:56 +0000

    task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Basic annotations - to_kv"""
-from typing import Union
+from typing import Union, Tuple
 
-
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple"""
     return (k, v * v)
 
