11103911e6282789eec28a4e8dac6ced0e2ece18
soO <444deph12@gmail.com>
2023-10-04 17:35:45 +0000

    B_a

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+""" Module auth
+"""
 from flask import request
 from typing import List, TypeVar
 
@@ -9,13 +10,7 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require_auth method
-        Determines if a route requires authentication or not
-        Args:
-            path (str): the path of the request
-            excluded_paths (List[str]): a list of paths that do not require authentication
-        Returns:
-            bool: True if authentication is required, False otherwise
+        """ require_auth
         """
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
@@ -30,23 +25,13 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ authorization_header method
-        Returns the value of the Authorization header in a request
-        Args:
-            request (flask.Request): the request object
-        Returns:
-            str: the value of the Authorization header, or None if it is not present
+        """ authorization_header
         """
         if request is None or 'Authorization' not in request.headers:
             return None
         return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ current_user method
-        Returns the current user making the request
-        Args:
-            request (flask.Request): the request object
-        Returns:
-            TypeVar('User'): the current user, or None if there is no user
+        """ current_user
         """
         return None
