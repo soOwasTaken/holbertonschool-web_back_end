97c57a6ca16e108ee6f79deecdfeb4c4cd991815
soO <444deph12@gmail.com>
2023-10-06 06:22:33 +0000

    cookiiies

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -68,11 +68,8 @@ def before_request_handler():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) and auth.session_cookie(request) is None:
         abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)
 
 
 @app.errorhandler(404)
