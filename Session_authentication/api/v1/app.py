a19388bf7900dbdde8484254b5c52a4d21d6f11c
soO <444deph12@gmail.com>
2023-10-06 06:07:24 +0000

    renaming file properly lol

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -27,7 +27,23 @@ def before_request_func():
     """
     Handle actions before request
     """
+    if auth is None:
+        return
+    
     request.current_user = auth.current_user(request)
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
 
 
 def before_request_handler():
