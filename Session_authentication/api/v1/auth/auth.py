db9d9bff6b35ecd2e6e6988ee29d12ad2f0b3485
soO <444deph12@gmail.com>
2023-10-06 06:17:47 +0000

    cookiiies

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -24,6 +25,14 @@ class Auth:
             elif path == excluded:
                 return False
         return True
+
+    def session_cookie(self, request=None):
+        """
+        Returns a cookie value from a request
+        """
+        if request is None:
+            return None
+        return request.cookies.get(getenv('SESSION_NAME', ''))
 
     def authorization_header(self, request=None) -> str:
         """authorization_header
