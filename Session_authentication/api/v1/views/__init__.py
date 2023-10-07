0a976be6358d2a7ee151427771cf8b14f8873281
soO <444deph12@gmail.com>
2023-10-07 00:45:49 +0000

    t

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -2,13 +2,14 @@
 """ DocDocDocDocDocDoc
 """
 from flask import Flask, Blueprint
-from api.v1.views.session_auth import *
+from models.user import User
+
 
 app = Flask(__name__)
-from . import session_auth
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
+from api.v1.views.session_auth import *
 from api.v1.views.index import *
 from api.v1.views.users import *
 
