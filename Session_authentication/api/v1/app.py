61bb012c1c0c277f3e219ffe35c2f7c365cb4313
soO <444deph12@gmail.com>
2023-10-07 01:33:20 +0000

    t

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -20,6 +20,9 @@ if AUTH_TYPE == 'auth':
 elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(401)
@@ -48,13 +51,16 @@ def before_request() -> str:
         return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if not auth.authorization_header(request)\
+       and not auth.session_cookie(request):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
