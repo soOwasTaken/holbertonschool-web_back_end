df2297d13ba7bd84f4485dcd8efbf7f86e7546e0
soO <444deph12@gmail.com>
2023-10-10 08:29:35 +0000

    pycodestyle

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -23,6 +23,23 @@ def register_user():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Check if a user exists and validate the password."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or password is None:
+        abort(400)
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
