43d143a20c28434d102c1f1bf801b467a73f84ef
soO <444deph12@gmail.com>
2023-10-10 08:15:01 +0000

    updated auth.py

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python3
 """DB AUTH"""
+
 import bcrypt
+from db import DB
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
-def _hash_password(password: str) -> bytes:
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    Hash a password using bcrypt, return the hashed password as bytes.
-    """
-    salt = bcrypt.gensalt()
 
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    def __init__(self):
+        self._db = DB()
 
-    return hashed_password
+    @staticmethod
+    def _hash_password(password: str) -> bytes:
+        """
+        Hash a password using bcrypt, return the hashed password as bytes.
+        """
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user and return the User object."""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            return self._db.add_user(email, hashed_password)
+        else:
+            raise ValueError(f'User {email} already exists')
