0580b6a79cf85c995bfbd75252162e85b31d8917
soO <444deph12@gmail.com>
2023-10-10 08:04:52 +0000

    add of find_user_by in db.py

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,8 +5,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
-from user import Base, User  # ensure to import User class from the user module
+from user import Base, User
 
 
 class DB:
@@ -37,3 +39,15 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find and return the first User by the input arguments.
+        """
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound(
+                "No result found for the provided search criteria.")
+        except InvalidRequestError:
+            raise InvalidRequestError(
+                "Invalid query parameters were provided.")
