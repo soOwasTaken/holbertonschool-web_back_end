818fc41c84175fdd8d0dd86013a0a080766cbf19
soO <444deph12@gmail.com>
2023-10-10 08:21:14 +0000

    add of flask, fixing _hash_password

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 """flask module
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route('/', methods=['GET'])
-def welcome():
-    """Welcome route."""
-    return jsonify({"message": "Bienvenue"}), 200
+@app.route('/users', methods=['POST'])
+def register_user():
+    """Register a user and respond with a status message."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or password is None:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
