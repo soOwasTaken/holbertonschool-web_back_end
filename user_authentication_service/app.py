037a8c53aedb89ca1a801b477b6aea18dd2057e8
soO <444deph12@gmail.com>
2023-10-10 08:36:15 +0000

    app & auth password routes

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -42,5 +42,72 @@ def login():
         abort(401)
 
 
+def logout():
+    """Find the user with the requested session ID. If the user exists destroy
+    the session and redirect the user to GET /. If the user does not exist,
+    respond with a 403 HTTP status.
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
+
+
+@app.route('/profile', methods=['GET'])
+def profile():
+    """Find the user with the requested session ID. If the user exists, respond
+    with a 200 HTTP status and a JSON payload of the user's email. If the user
+    does not exist, respond with a 403 HTTP status.
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """Generate a reset password token and respond with a 200 HTTP status and a
+    JSON payload of the reset token. If the email is not registered, respond
+    with a 403 HTTP status.
+    """
+    email = request.form.get('email')
+
+    if email is None:
+        abort(403)
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
+def update_password():
+    """Update a user's password and respond with a 200 HTTP status and a JSON
+    payload of the user's email. If the reset token is invalid, respond with a
+    403 HTTP status.
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    if email is None or reset_token is None or new_password is None:
+        abort(403)
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -71,3 +71,33 @@ class Auth:
             return None
         else:
             return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy a session ID."""
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except ValueError:
+            pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate a reset password token."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update a user's password."""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        else:
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id,
+                                 hashed_password=hashed_password,
+                                 reset_token=None)
