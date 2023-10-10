2d0ea35280c193ba86ce03019d359abde9b22531
soO <444deph12@gmail.com>
2023-10-10 08:30:53 +0000

    pycodestyle

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -23,6 +23,7 @@ def register_user():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+
 @app.route('/sessions', methods=['POST'])
 def login():
     """Check if a user exists and validate the password."""
--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -60,3 +60,14 @@ class Auth:
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """Get a user from a session ID."""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        else:
+            return user
