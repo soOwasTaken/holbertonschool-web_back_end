f8cabb6f9d8f8c597037312f6d6a301c29fcb507
soO <444deph12@gmail.com>
2023-10-18 11:17:37 +0000

    Unnitest 2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -15,6 +15,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
+        """ method to test that the method returns what it is supposed to """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
     @parameterized.expand([
