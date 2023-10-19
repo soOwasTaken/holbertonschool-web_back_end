52c0d3fd1c74cb2803c0ae9cc4c157635f8cd599
soO <444deph12@gmail.com>
2023-10-19 13:17:25 +0000

    task 3

---
+++ i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Basic Flask app """""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Babel configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+@babel.localeselector
+def get_locale():
+    """ Get locale from request """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ Basic Flask app"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
