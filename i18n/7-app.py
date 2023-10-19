be2e649ec63054eed3190e4589f898512127bb3e
soO <444deph12@gmail.com>
2023-10-19 13:37:54 +0000

    i18n finished

--- i18n/7-app.py
+++ i18n/7-app.py
@@ -2,18 +2,9 @@
 """ Basic Flask app """""
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
-import pytz
 
 app = Flask(__name__)
 babel = Babel(app)
-
-
-SUPPORTED_TIMEZONES = [
-    "Europe/Paris",
-    "US/Central",
-    "Vulcan",
-    "Europe/London",
-]
 
 
 class Config:
@@ -47,28 +38,6 @@ def before_request():
     g.user = get_user()
 
 
-@babel.timezoneselector
-def get_timezone():
-    """ Get timezone from request"""
-    url_timezone = request.args.get('timezone')
-
-    if hasattr(g, 'user') and g.user and 'timezone' in g.user:
-        user_timezone = g.user['timezone']
-        if user_timezone in SUPPORTED_TIMEZONES:
-            return user_timezone
-
-    if url_timezone:
-        try:
-            if url_timezone in SUPPORTED_TIMEZONES:
-                return url_timezone
-            else:
-                raise pytz.exceptions.UnknownTimeZoneError
-        except pytz.exceptions.UnknownTimeZoneError:
-            pass
-
-    return 'UTC'
-
-
 @babel.localeselector
 def get_locale():
     """ to determine the best match with our supported languages """
@@ -93,7 +62,7 @@ app.config.from_object(Config)
 @app.route('/')
 def index():
     """ Basic Flask app"""
-    return render_template('7-index.html')
+    return render_template('6-index.html')
 
 
 if __name__ == '__main__':
