439bc4a0b5c48ef396808c0a062ad814e1315e83
soO <444deph12@gmail.com>
2023-11-01 16:04:12 +0000

    task5

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- valid_email.sql
+-- create trigger boolean
+
+DELIMITER |
+CREATE TRIGGER email_bool BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+    SET NEW.valid_email = 0;
+    END IF;
+END;
+|
