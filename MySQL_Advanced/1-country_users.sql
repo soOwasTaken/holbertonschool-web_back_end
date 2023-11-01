aec28358b95365aefeb95d5dde5626314db91411
soO <444deph12@gmail.com>
2023-11-01 15:40:48 +0000

    task 1

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- Task : 1. In and not out 
+-- script can be executed on any database
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NUµLL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
