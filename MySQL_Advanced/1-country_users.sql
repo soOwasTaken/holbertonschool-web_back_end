0f925d90411ce8523554e9b88f9e14dfdab49d38
soO <444deph12@gmail.com>
2023-11-01 15:51:07 +0000

    task 1

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -5,8 +5,6 @@ CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    country ENUM('US', 'CO', 'TN') NOT NULL,
     PRIMARY KEY (id)
 );
-
-ALTER TABLE users ADD COLUMN IF NOT EXISTS country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US';
