7629e8ae1397a18bd6167f5fb280afe1761d3d2c
soO <444deph12@gmail.com>
2023-11-05 17:22:13 +0000

    fixing div

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,15 +1,16 @@
 -- 10-div.sql
-DELIMITER //
 
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS DECIMAL(10, 4)
+DELIMITER |
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
 BEGIN
-    DECLARE result DECIMAL(10, 4);
-    IF b = 0 THEN
-        SET result = 0;
-    ELSE
-        SET result = a / b;
-    END IF;
-    RETURN result;
-END //
-
-DELIMITER ;
+  DECLARE result FLOAT;
+  IF b = 0 THEN
+    SET result = 0;
+  ELSE
+    SET result = a / b;
+  END IF;
+  RETURN result;
+END;
+|
