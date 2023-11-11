ea866a97f314ece4157dd2f528169ffba427686f
soO <444deph12@gmail.com>
2023-11-11 01:41:23 -0800

    fixing eslint

--- ES6_basic/12-main.js
+++ ES6_basic/12-main.js
@@ -1,9 +1,9 @@
-import createEmployeesObject from "./11-createEmployeesObject.js";
-import createReportObject from "./12-createReportObject.js";
+import createEmployeesObject from './11-createEmployeesObject.js';
+import createReportObject from './12-createReportObject.js';
 
 const employees = {
-  ...createEmployeesObject("engineering", ["Bob", "Jane"]),
-  ...createEmployeesObject("marketing", ["Sylvie"]),
+  ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+  ...createEmployeesObject('marketing', ['Sylvie']),
 };
 
 const report = createReportObject(employees);
