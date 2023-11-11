4747f1995d77785f53649298471341fee4f96439
soO <444deph12@gmail.com>
2023-11-11 01:55:33 -0800

    fixing js

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -4,13 +4,12 @@ export function taskFirst() {
 }
 
 export function getLast() {
-  return 'is okay';
+  return ' is okay';
 }
 
 export function taskNext() {
-  let combination = "But sometimes let ";
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
 }
-
