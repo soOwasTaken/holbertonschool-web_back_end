4d9a7bdbe6c9600174f612d6d98c5088229c2a59
soO <444deph12@gmail.com>
2023-11-16 10:37:35 +0000

    tring another way

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -7,5 +7,5 @@ export default function handleProfileSignup() {
       const { firstName, lastName } = resp[1];
       console.log(`${body} ${firstName} ${lastName}`);
     })
-    .catch(() => console.log("Signup system offline"));
+    .catch(() => console.log('Signup system offline'));
 }
