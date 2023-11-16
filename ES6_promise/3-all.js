387870a075c9e4387a40a9240e7694a44f66e550
soO <444deph12@gmail.com>
2023-11-16 10:36:35 +0000

    tring another way

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-export function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
+    .then((resp) => {
+      const { body } = resp[0];
+      const { firstName, lastName } = resp[1];
+      console.log(`${body} ${firstName} ${lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => console.log("Signup system offline"));
 }
-
-export default handleProfileSignup;
