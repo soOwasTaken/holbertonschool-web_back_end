5a0e24b38a79e15f0bd5775dc273910e8621c015
soO <444deph12@gmail.com>
2023-11-16 10:31:50 +0000

    fixing linter

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,14 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      for (const value of values) {
-        if (value.status === 'rejected') {
-          value.value = `Error: ${value.reason.message}`;
-          delete value.reason;
-        }
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    for (const value of values) {
+      if (value.status === 'rejected') {
+        value.value = `Error: ${value.reason.message}`;
+        delete value.reason;
       }
-      return values;
-    });
+    }
+    return values;
+  });
 }
