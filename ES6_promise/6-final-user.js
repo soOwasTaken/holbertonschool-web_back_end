81c3666fa810427fc2c67bbcda46b0572c5b4ce8
soO <444deph12@gmail.com>
2023-11-16 10:29:11 +0000

    fixing final user

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,13 +2,14 @@ import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) =>
-    results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }))
-  );
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => {
+      for (const value of values) {
+        if (value.status === 'rejected') {
+          value.value = `Error: ${value.reason.message}`;
+          delete value.reason;
+        }
+      }
+      return values;
+    });
 }
