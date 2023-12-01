3d28a9bf16b1bf8c172048b9071e844624012f31
soO <444deph12@gmail.com>
2023-12-01 16:02:59 +0000

    test

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,40 +3,31 @@ const { readDatabase } = require('../utils');
 class StudentsController {
   static async getAllStudents(req, res) {
     try {
-      const data = await readDatabase(req.databasePath);
-
-      let responseMessage = 'This is the list of our students\n';
-      for (const field in data) {
-        if (Object.prototype.hasOwnProperty.call(data, field)) {
-          responseMessage += `Number of students in ${field}: ${
-            data[field].count
-          }. List: ${data[field].list.join(', ')}\n`;
-        }
+      const students = await readDatabase(process.argv[2]);
+      let response = "This is the list of our students\n";
+      for (const [field, names] of Object.entries(students)) {
+        response += `Number of students in ${field}: ${
+          names.length
+        }. List: ${names.join(", ")}\n`;
       }
-      res.status(200).send(responseMessage);
+      return res.status(200).send(response);
     } catch (error) {
-      res.status(500).send('Cannot load the database');
+      return res.status(500).send("Cannot load the database");
     }
   }
 
   static async getAllStudentsByMajor(req, res) {
-    const major = req.params.major.toUpperCase();
-    if (major !== 'CS' && major !== 'SWE') {
-      return res.status(500).send('Major parameter must be CS or SWE');
-    }
-
     try {
-      const data = await readDatabase(req.databasePath);
-
-      const responseMessage = `List of students in ${major}: ${data[
-        major
-      ].list.join(', ')}`;
-
-      res.status(200).send(responseMessage);
+      const major = req.params.major.toUpperCase();
+      if (!["CS", "SWE"].includes(major)) {
+        return res.status(500).send("Major parameter must be CS or SWE");
+      }
+      const students = await readDatabase(process.argv[2]);
+      const names = students[major] || [];
+      return res.status(200).send(`List: ${names.join(", ")}`);
     } catch (error) {
-      res.status(500).send('Cannot load the database');
+      return res.status(500).send("Cannot load the database");
     }
-    return null;
   }
 }
 
