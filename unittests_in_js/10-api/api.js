b3a371bdf0e8c281fffd1a41af614b2d3b15ed3b
soO <444deph12@gmail.com>
2023-12-02 09:33:25 +0000

    fixing 10

--- unittests_in_js/10-api/api.js
+++ unittests_in_js/10-api/api.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json()); // Middleware to parse JSON bodies
+// Middleware to parse JSON bodies
+app.use(express.json());
 
+// Root endpoint
 app.get("/", (req, res) => {
   res.send("Welcome to the payment system");
 });
 
+// Endpoint for cart with number validation
 app.get("/cart/:id(\\d+)", (req, res) => {
   const id = req.params.id;
   res.send(`Payment methods for cart ${id}`);
 });
 
+// New endpoint for available_payments
 app.get("/available_payments", (req, res) => {
   res.json({
     payment_methods: {
@@ -21,14 +25,16 @@ app.get("/available_payments", (req, res) => {
   });
 });
 
+// New endpoint for POST /login
 app.post("/login", (req, res) => {
-  const username = req.body.userName;
-  res.send(`Welcome ${username}`);
+  res.send(`Welcome ${req.body.userName}`);
 });
 
+// Define the port and start listening
 const PORT = 7865;
 app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
 });
 
+// Export the app for testing
 module.exports = app;
--- unittests_in_js/10-api/api.test.js
+++ unittests_in_js/10-api/api.test.js
@@ -2,71 +2,72 @@ const chai = require("chai");
 const request = require("request");
 const expect = chai.expect;
 
-describe("Index page", () => {
-  it("should return correct status code", (done) => {
-    request("http://localhost:7865", (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-  it("should return 'Welcome to the payment system'", (done) => {
-    request("http://localhost:7865", (error, response, body) => {
-      expect(body).to.equal("Welcome to the payment system");
-      done();
-    });
-  });
-});
-
-describe("Cart page", () => {
-  it("should return correct status code and result for a number id", (done) => {
-    request("http://localhost:7865/cart/12", (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal("Payment methods for cart 12");
-      done();
-    });
-  });
-
-  it("should return 404 status code for a non-number id", (done) => {
-    request("http://localhost:7865/cart/hello", (error, response, body) => {
-      expect(response.statusCode).to.equal(404);
-      done();
-    });
-  });
-});
-
-describe("/available_payments endpoint", () => {
-  it("should return the correct payment methods object", (done) => {
-    request(
-      "http://localhost:7865/available_payments",
-      (error, response, body) => {
+describe("API Tests", () => {
+  // Test for the root endpoint
+  describe('Root Endpoint "/"', () => {
+    it("should return correct status code and welcome message", (done) => {
+      request("http://localhost:7865", (error, response, body) => {
         expect(response.statusCode).to.equal(200);
-        expect(JSON.parse(body)).to.deep.equal({
-          payment_methods: {
-            credit_cards: true,
-            paypal: false,
-          },
-        });
+        expect(body).to.equal("Welcome to the payment system");
         done();
-      }
-    );
+      });
+    });
   });
-});
 
-describe("/login endpoint", () => {
-  it("should return the welcome message for a user", (done) => {
-    const options = {
-      url: "http://localhost:7865/login",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userName: "Betty" }),
-    };
+  // Test suite for cart endpoint
+  describe('Cart Endpoint "/cart/:id"', () => {
+    it("should return correct status code and result for a number id", (done) => {
+      request("http://localhost:7865/cart/12", (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal("Payment methods for cart 12");
+        done();
+      });
+    });
 
-    request(options, (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal("Welcome Betty");
-      done();
+    it("should return 404 status code for a non-number id", (done) => {
+      request("http://localhost:7865/cart/hello", (error, response, body) => {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  // Test suite for available_payments endpoint
+  describe('Available Payments Endpoint "/available_payments"', () => {
+    it("should return correct payment methods", (done) => {
+      request(
+        "http://localhost:7865/available_payments",
+        (error, response, body) => {
+          expect(response.statusCode).to.equal(200);
+          expect(JSON.parse(body)).to.deep.equal({
+            payment_methods: {
+              credit_cards: true,
+              paypal: false,
+            },
+          });
+          done();
+        }
+      );
+    });
+  });
+
+  // Test suite for POST /login endpoint
+  describe('Login Endpoint "POST /login"', () => {
+    it("should welcome the user", (done) => {
+      const options = {
+        url: "http://localhost:7865/login",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userName: "Betty" }),
+      };
+
+      request(options, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal("Welcome Betty");
+        done();
+      });
     });
   });
 });
