e84172c2ece0251d9d2a4918bed52a6dc06987d2
soO <444deph12@gmail.com>
2023-12-02 09:17:16 +0000

    ? i have rounded

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -4,8 +4,9 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', () => {
     it('should return rounded sum of two numbers', () => {
         assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
+        assert.strictEqual(calculateNumber(1, 3.2), 4);
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+        // Add more test cases as needed
     });
 });
